8ef742666af25699e7bcf92272166e7bea124ad8
Carlos Bracho <brachocarlos152@gmail.com>
2026-04-17 21:23:58 -0400

    Add cross-env dependency and update scripts for development and production environments

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 // Import dependencies
 import express from "express";
 import dotenv from "dotenv";
+import path from "path";
 
 // Import database connection function
 import { connectDB } from "./config/db.js";
@@ -22,11 +23,25 @@ const app = express();
 // Set the port from environment variable or default to 5000
 const PORT = process.env.PORT || 5000;
 
+// Get the current directory path using path.resolve() for compatibility with ES modules
+const __dirname = path.resolve(); // Get the current directory path
+
 app.use(express.json()); //allows us to parse JSON bodies in requests
 
 // Use the imported route handlers for products and users
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
+
+// Serve static files from the frontend in production
+if (process.env.NODE_ENV === "production") {
+  // Serve static files from the React frontend build directory
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+  // For any route that doesn't match the API routes, serve the React frontend's index.html
+  app.get("/{*splat}", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
 
 // NOTE: Uncomment this route to test if the server is running correctly
 // app.get("/", (req, res) => {
